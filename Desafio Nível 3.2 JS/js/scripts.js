f95fccbe780a1dcaec5e561e481da98a986e9055
Artur Mendes <arturm.israel@gmail.com>
2026-07-13 09:17:32 -0300

    new-projects

---
+++ Desafio Nível 3.2 JS/js/scripts.js
@@ -0,0 +1,50 @@
+const form = document.querySelector("#form-inputs");
+const inputCidade = document.querySelector("#city");
+
+const climateInfo = document.querySelector("#climate-info");
+
+const cidade = document.querySelector("#cidade");
+const temperatura = document.querySelector("#temperatura");
+const sensacao = document.querySelector("#sensacao");
+const umidade = document.querySelector("#umidade");
+const descricao = document.querySelector("#descricao");
+const vento = document.querySelector("#vento");
+
+const chave = "586b143a19fd4831bf9cb10cbb00dee7";
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  buscarClima();
+});
+
+async function buscarClima() {
+  const nomeCidade = inputCidade.value;
+
+  if (nomeCidade === "") {
+    return;
+  }
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${nomeCidade}&appid=${chave}&units=metric&lang=pt_br`;
+
+  console.log(url);
+
+  const resposta = await fetch(url);
+  const dados = await resposta.json();
+  console.log(dados);
+
+  if (dados.cod === "404") {
+    alert("Erro! Cidade Não Encontrada.");
+    return;
+  }
+
+  cidade.innerText = `${dados.name}`;
+  temperatura.innerText = `${dados.main.temp} °C`;
+  sensacao.innerText = `${dados.main.feels_like} °C`;
+  umidade.innerText = `${dados.main.humidity} %`;
+  descricao.innerText = `${dados.weather[0].description}`;
+  vento.innerText = `${dados.wind.speed} km/h`;
+
+  climateInfo.classList.remove("hide");
+
+  inputCidade.value = ""
+}
